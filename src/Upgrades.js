dc92ca6feaa7c974963e41880070222fc6631f96
莫子琳 <molly@mozilindeMacBook-Air.local>
2022-12-27 00:02:31 +0800

    add a Upgrades.js

---
+++ src/Upgrades.js
@@ -0,0 +1,28 @@
+const Upgrades =() => {
+    return(
+        <div id="square">
+        <div class="bonus">
+            <div class="upgrades">
+                <div class="upgrades-ch">升級</div>
+                <div class="upgrades-en">UPGRADES</div>
+            </div>
+            <div class="bonus-name-and-price">
+                <div class="expresso">
+                    <div class="bonus-name">閃電濃縮 EXTRA EXPRESSO SHOT</div>
+                    <div class="bonus-price">10</div>
+                </div>
+                <div class="oatlymilk">
+                    <div class="bonus-name">OATLY燕麥奶 OATLY MILK</div>
+                    <div class="bonus-price">20</div>
+                </div>
+                <div class="coco">
+                    <div class="bonus-name">榛果可可醬 NUTELLA (10g)</div>
+                    <div class="bonus-price">5</div>
+                </div>  
+            </div>
+        </div>   
+    </div>
+    )
+}
+
+export default Upgrades;
