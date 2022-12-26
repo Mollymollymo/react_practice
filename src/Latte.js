cedc9b9e5e56d4d319bb52ec50c028cf6c8c30f7
莫子琳 <molly@mozilindeMacBook-Air.local>
2022-12-26 23:51:08 +0800

    add a Latte,js

---
+++ src/Latte.js
@@ -0,0 +1,79 @@
+const Latte =() =>{
+    return(
+        <><div className="oatly">
+            <div className="name-and-degree">
+                <div className="name">
+                    <div className="name-ch">OATLY燕麥拿鐵</div>
+                    <div className="name-en">OATLY LATTE</div>
+                </div>
+                <div className="degree">HOT/ICED</div>
+            </div>
+            <div className="price-block">
+                <div className="regular-price">80</div>
+                <div className="upsize-price">105</div>
+            </div>
+        </div><div className="macadamia">
+                <div className="name-and-degree">
+                    <div className="name">
+                        <div className="name-ch">夏威夷果拿鐵</div>
+                        <div className="name-en">MACADAMIA LATTE</div>
+                    </div>
+                    <div className="degree">HOT/ICED</div>
+                </div>
+                <div className="price-block">
+                    <div className="regular-price">80</div>
+                    <div className="upsize-price">105</div>
+                </div>
+            </div><div className="nutella">
+                <div className="name-and-degree">
+                    <div className="name">
+                        <div className="name-ch">榛心可可拿鐵</div>
+                        <div className="name-en">NUTELLA LATTE</div>
+                    </div>
+                    <div className="degree">HOT/ICED</div>
+                </div>
+                <div className="price-block">
+                    <div className="regular-price">80</div>
+                    <div className="upsize-price">105</div>
+                </div>
+            </div><div className="osmanthus">
+                <div className="name-and-degree">
+                    <div className="name">
+                        <div className="name-ch">桂花拿鐵</div>
+                        <div className="name-en">OSMANTHUS LATTE</div>
+                    </div>
+                    <div className="degree">HOT/ICED</div>
+                </div>
+                <div className="price-block">
+                    <div className="regular-price">80</div>
+                    <div className="upsize-price">105</div>
+                </div>
+            </div><div className="beach">
+                <div className="name-and-degree">
+                    <div className="name">
+                        <div className="name-ch">沙灘拿鐵</div>
+                        <div className="name-en">BEACH LATTE</div>
+                    </div>
+                    <div className="degree">HOT/ICED</div>
+                </div>
+                <div className="price-block">
+                    <div className="regular-price">80</div>
+                    <div className="upsize-price">105</div>
+                </div>
+            </div><div className="icepresso">
+                <div className="name-and-degree">
+                    <div className="name">
+                        <div className="name-ch">冰煉乳拿鐵</div>
+                        <div className="name-en">ICEPRESSO LATTE</div>
+                    </div>
+                    <div className="degree">ICED</div>
+                </div>
+                <div className="price-block">
+                    <div className="regular-price">80</div>
+                    <div className="upsize-price">105</div>
+                </div>
+            </div></>
+    )
+}
+
+export default Latte;
