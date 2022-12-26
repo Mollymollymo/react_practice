d57e4c89ca4d80d1ffce31199e9ebfd664398128
莫子琳 <molly@mozilindeMacBook-Air.local>
2022-12-27 00:01:29 +0800

    add a Milks.js

---
+++ src/Milks.js
@@ -0,0 +1,62 @@
+const Milks =() => {
+    return(
+        <><div className="title-container">
+        <div className="title-block">
+            <div className="title-ch">歐蕾系列</div>
+            <div className="title-en">MILKS</div>
+        </div>
+        <div className="capacity-block">
+            <div className="regular-block">
+                <div className="regular-title-ch">中杯</div>
+                <div className="regular-tutee-en">REGULAR</div>
+            </div>
+            <div className="upside-block">
+                <div className="usize-title-ch">大杯</div>
+                <div className="upsize-yiyle-en">UPSIZE</div>
+            </div>
+        </div>
+    </div>
+    <div className="line"></div>
+    <div className="chocolate">
+        <div className="name-and-degree">
+            <div className="name">
+                <div className="name-ch">濃郁式可可</div>
+                <div className="name-en">CHOCOLATE</div>
+            </div>
+            <div className="degree">HOT/ICED</div>
+        </div>
+        <div className="price-block">
+            <div className="regular-price">60</div>
+            <div className="upsize-price">80</div>
+        </div>
+</div>
+    <div className="matcha">
+        <div className="name-and-degree">
+            <div className="name">
+                <div className="name-ch">抹茶歐蕾</div>
+                <div className="name-en">MATCHA LATTE</div>
+            </div>
+        <div className="degree">HOT/ICED</div>
+        </div>
+        <div className="price-block">
+        <div className="regular-price">70</div>
+        <div className="upsize-price">90</div>
+        </div>
+    </div>
+    <div className="nutella">
+        <div className="name-and-degree">
+            <div className="name">
+                <div className="name-ch">抹茶夏威夷果歐蕾</div>
+                <div className="name-en">NUTELLA LATTE</div>
+            </div>
+        <div className="degree">HOT/ICED</div>
+        </div>
+    <div className="price-block">
+        <div className="regular-price">90</div>
+        <div className="upsize-price">110</div>
+        </div>
+</div></>
+    )
+}
+
+export default Milks;
